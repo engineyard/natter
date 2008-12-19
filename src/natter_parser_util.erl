db9145e7ba3973cacba88e43bd1d4228c88a87db
Kevin A. Smith <ksmith@engineyard.com>
2008-12-19 12:24:21 -0500

    * Adding LGPL license info to all source and adding LICENSE file

--- src/natter_parser_util.erl
+++ src/natter_parser_util.erl
@@ -1,3 +1,21 @@
+
+% Copyright 2008, Engine Yard, Inc.
+%
+% This file is part of Natter.
+%
+% Natter is free software: you can redistribute it and/or modify it under the
+% terms of the GNU Lesser General Public License as published by the Free
+% Software Foundation, either version 3 of the License, or (at your option) any
+% later version.
+%
+% Natter is distributed in the hope that it will be useful, but WITHOUT ANY
+% WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+% A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+% details.
+%
+% You should have received a copy of the GNU Lesser General Public License
+% along with Natter.  If not, see <http://www.gnu.org/licenses/>.
+
 -module(natter_parser_util).
 
 -export([sanitize/1, escape/1, clean_whitespace/1]).
